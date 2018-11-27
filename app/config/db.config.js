0b4d525163626e1ef3928dc0184e13779069e44d
Tân Nguyễn <nhattanhutech@gmail.com>
2018-11-27 16:47:26 +0700

    one_to_many

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,16 +2,16 @@ const env = require('./env.js');
 
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(env.database, env.username, env.password, {
-  host: env.host,
-  dialect: env.dialect,
-  operatorsAliases: false,
+    host: env.host,
+    dialect: env.dialect,
+    operatorsAliases: false,
 
-  pool: {
-    max: env.max,
-    min: env.pool.min,
-    acquire: env.pool.acquire,
-    idle: env.pool.idle
-  }
+    pool: {
+        max: env.max,
+        min: env.pool.min,
+        acquire: env.pool.acquire,
+        idle: env.pool.idle
+    }
 });
 
 const db = {};
@@ -19,8 +19,11 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-//Models/tables
-db.customers = require('../model/customer.model.js')(sequelize, Sequelize);
+db.company = require('../model/company.model.js')(sequelize, Sequelize);
+db.product = require('../model/product.model.js')(sequelize, Sequelize);
 
+// Here we can connect companies and products base on company'id
+db.company.hasMany(db.product, { foreignKey: 'fk_companyid', sourceKey: 'Id' });
+db.product.belongsTo(db.company, { foreignKey: 'fk_companyid', targetKey: 'Id' });
 
 module.exports = db;
